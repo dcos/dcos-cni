f449877ecdf08abd3bb13ae24b7bb2aa987c3295
Avinash Sridharan <avinash@mesosphere.com>
2017-05-05 18:42:11 +0000

    Added capability to add minuteman interface to the `minuteman` package.

--- pkg/minuteman/plugin.go
+++ pkg/minuteman/plugin.go
@@ -7,10 +7,64 @@ import (
 	"log"
 	"os"
 
+	"github.com/containernetworking/cni/pkg/ns"
 	"github.com/containernetworking/cni/pkg/skel"
+
+	"github.com/vishvananda/netlink"
 )
 
 const DefaultPath = "/var/run/dcos/cni/l4lb"
+const IfName = "minuteman0"
+
+func setupInterface(netns string) error {
+	err := ns.WithNetNSPath(netns, func(_ ns.NetNS) error {
+		dummy := &netlink.Dummy{
+			LinkAttrs: netlink.LinkAttrs{
+				Name: IfName,
+			},
+		}
+
+		err := netlink.LinkAdd(dummy)
+		if err != nil {
+			return fmt.Errorf("failed to create dummy interface: %s", err)
+		}
+
+		// Bring up the interface
+		err = netlink.LinkSetUp(dummy)
+		if err != nil {
+			return fmt.Errorf("unable to bring the dummy interface up: %s", err)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("unable to configure minuteman interface in netns(%s): %s", netns, err)
+	}
+
+	return nil
+}
+
+func tearDownInterface(netns string) error {
+	err := ns.WithNetNSPath(netns, func(_ ns.NetNS) error {
+		iface, err := netlink.LinkByName(IfName)
+		if err != nil {
+			return fmt.Errorf("failed to lookup %s: %s", IfName, err)
+		}
+
+		if err = netlink.LinkDel(iface); err != nil {
+			return fmt.Errorf("failed to delete %s: %s", IfName, err)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("unable to remove minuteman interface in netns(%s): %s", netns, err)
+	}
+
+	return nil
+}
 
 type Error string
 
@@ -34,12 +88,18 @@ func CniAdd(args *skel.CmdArgs) error {
 		return Error(fmt.Sprintf("couldn't create directory for storing minuteman container registration information:%s", err))
 	}
 
-	fmt.Fprintln(os.Stderr, "Registering netns for containerID", args.ContainerID, " at path: ", conf.Path)
+	log.Println("Registering netns for containerID", args.ContainerID, " at path: ", conf.Path)
 
 	// Create a file with name `ContainerID` and write the network
 	// namespace into this file.
 	if err := ioutil.WriteFile(conf.Path+"/"+args.ContainerID, []byte(args.Netns), 0644); err != nil {
 		return Error(fmt.Sprintf("couldn't checkout point the network namespace for containerID:%s for minuteman", args.ContainerID))
+	}
+
+	log.Println("Creating minuteman interface ", IfName)
+	// Create a `minuteman` interface.
+	if err := setupInterface(args.Netns); err != nil {
+		return fmt.Errorf("failure in creating minuteman interface: %s", err)
 	}
 
 	return nil
@@ -60,6 +120,12 @@ func CniDel(args *skel.CmdArgs) error {
 	// Remove the container registration.
 	if err := os.Remove(conf.Path + "/" + args.ContainerID); err != nil {
 		log.Printf("Unable to remove registration for contianerID:%s from minuteman", args.ContainerID)
+	}
+
+	log.Println("Removing minuteman interface ", IfName)
+	// Deleate the `minuteman` interface.
+	if err := tearDownInterface(args.Netns); err != nil {
+		return fmt.Errorf("failure in deleting the minuteman interface: %s", err)
 	}
 
 	return nil
