168017cbb4d24d50a4510909c9fcf636ca8c746b
Avinash Sridharan <avinash.sridharan@gmail.com>
2017-09-14 16:01:38 +0000

    Added `CNI_PLUGIN_PATH` variable to setup CNI plugins for tests.

--- cmd/l4lb/l4lb_suite_test.go
+++ cmd/l4lb/l4lb_suite_test.go
@@ -4,10 +4,26 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"os"
+
 	"testing"
 )
+
+var osPath string
 
 func TestL4lb(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "L4lb Suite")
 }
+
+var _ = BeforeSuite(func() {
+	osPath = os.Getenv("PATH")
+	cniPath, present := os.LookupEnv("CNI_PLUGIN_PATH")
+	Expect(present).To(Equal(true), "We cannot run these tests without a `CNI_PLUGIN_PATH` set")
+
+	os.Setenv("PATH", cniPath+":"+osPath)
+})
+
+var _ = AfterSuite(func() {
+	os.Setenv("PATH", osPath)
+})
