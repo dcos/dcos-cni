d0f6753109aa1dcf00faf213abaa2b73f10fa530
Deepak Goel <dgoel@mesosphere.com>
2018-02-21 12:13:41 -0800

    adds IP address needed by DNS proxy in kubernetes on DC/OS (#8)
    Kubernetes on DC/OS runs a DNS proxy that listen on spartan interface.
    This patch adds the IP address to the plugin so that appropriate routes
    are created in the container using this plugin

--- pkg/spartan/config.go
+++ pkg/spartan/config.go
@@ -40,6 +40,10 @@ var IPs = []net.IPNet{
 		IP:   net.IPv4(198, 51, 100, 3),
 		Mask: net.IPv4Mask(0xff, 0xff, 0xff, 0xff),
 	},
+	net.IPNet{
+		IP:   net.IPv4(198, 51, 100, 4), //used by k8s DNS proxy
+		Mask: net.IPv4Mask(0xff, 0xff, 0xff, 0xff),
+	},
 }
 
 // TODO(asridharan): This needs to be derived from the spartan
